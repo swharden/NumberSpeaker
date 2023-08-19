875f196a7168977a833737c837d7cfca951d6201
Scott W Harden <swharden@gmail.com>
2023-08-18 22:43:27 -0400

    dev: local test project

--- dev/TestSketch/TestSketch.ino
+++ dev/TestSketch/TestSketch.ino
@@ -5,9 +5,7 @@ void setup() {
 
 void loop() {
   for (;;) {
-    for (int i = 0; i < 10; i++) {
-      play_digit(i);
-    }
-    play_point();
+    String s = String("123.45678");
+    Speak(s);
   }
 }
