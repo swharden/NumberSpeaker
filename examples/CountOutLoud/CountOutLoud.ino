8ac051a98c5872ff9d0d049a9574e9e1bb13d9c5
Scott W Harden <swharden@gmail.com>
2023-09-17 13:36:31 -0400

    Create CountOutLoud.ino

---
+++ examples/CountOutLoud/CountOutLoud.ino
@@ -0,0 +1,15 @@
+#include "NumberSpeaker.h"
+
+NumberSpeaker numberSpeaker = NumberSpeaker();
+
+void setup() {
+  numberSpeaker.begin();  // speaker on pin 11
+}
+
+void loop() {
+  unsigned int count = 0;
+  for (;;) {
+    numberSpeaker.speak_int(count++);
+    delay(500);
+  }
+}
