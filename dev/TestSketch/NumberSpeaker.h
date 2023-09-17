14aace8f381aa00bd0ba3ef6299dda1712d80e31
Scott W Harden <swharden@gmail.com>
2023-09-17 12:15:38 -0400

    Update NumberSpeaker.h

--- dev/TestSketch/NumberSpeaker.h
+++ dev/TestSketch/NumberSpeaker.h
@@ -6,22 +6,75 @@
 
 class NumberSpeaker {
 public:
-  NumberSpeaker() {
-  }
+  NumberSpeaker() {}
 
   void begin() {
-    // Use timer 2 to generate an analog voltage using PWM on pin 11
+    // Use timer 2 to output PWM on pin 11
     pinMode(11, OUTPUT);
     TCCR2A |= bit(COM2A1);
     TCCR2B = bit(CS20);
     p_OCR = &OCR2A;
   }
 
+  void speak_int(uint32_t value) {
+    if (value == 0) {
+      speak_digit(0);
+      return;
+    }
+
+    uint32_t divisor = 1000000000;
+    uint8_t real_numbers_seen = 0;
+    while (divisor > 0) {
+      uint8_t digit = value / divisor;
+
+      if (digit > 0) {
+        real_numbers_seen = 1;
+      }
+
+      if (real_numbers_seen) {
+        speak_digit(digit);
+      }
+
+      value = value - (digit * divisor);
+      divisor /= 10;
+    }
+  }
+
+  void speak_float(float value) {
+    char buff[8];
+    dtostrf(value, 4, 6, buff);
+    for (int i = 0; i < sizeof(buff); i++) {
+      if (buff[i] == 0)
+        return;
+      speak_char(buff[i]);
+    }
+  }
+
+  void speak_string(String text) {
+    for (int i = 0; i < text.length(); i++) {
+      char c = text.charAt(i);
+      speak_char(c);
+    }
+  }
+
+  void speak_char(char c) {
+    if (c >= 48 && c <= 57) {
+      speak_digit(c - 48);
+    } else if (c == 46) {
+      speak_point();
+    } else {
+      speak_error();
+    }
+  }
+
+private:
+  uint8_t *p_OCR;  // pointer to the PWM register
+
   void play(uint8_t samples[], int length) {
     for (int i = 0; i < length; i++) {
       uint8_t value = pgm_read_byte(&samples[i]);
       *p_OCR = value;
-      delayMicroseconds(200);
+      delayMicroseconds(200);  // 5 kHz
     }
   }
 
@@ -32,26 +85,6 @@ public:
       delayMicroseconds(delay);
     }
   }
-
-  void speak(String text) {
-    for (int i = 0; i < text.length(); i++) {
-      char c = text.charAt(i);
-      if (c >= 48 && c <= 57) {
-        speak_digit(c - 48);
-      } else if (c == 46) {
-        speak_point();
-      } else {
-        // error tone
-      }
-    }
-  }
-
-  void speak(int value) {
-    speak_digit(value);
-  }
-
-private:
-  uint8_t *p_OCR;  // pointer to the PWM register
 
   void speak_error() {
     tone(500, 90);
